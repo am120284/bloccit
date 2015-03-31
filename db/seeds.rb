446aa9004e94dddb3d1b0e1bf68e4098a6d2fa84
Adrian <Adrian@Kraferd.com>
2015-03-31 16:32:05 -0700

    Complete Seed Data assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,24 @@
  require 'faker'
  
- # Create Posts
- 50.times do
-   Post.create!(
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
- posts = Post.all
- 
- # Create Comments
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: Faker::Lorem.paragraph
-   )
- end
- 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+ post_attr = [{title: "Post tets 03", body: "This is another test"}]
+ #Create Posts
+
+ post_attr.each do |attributes|
+     Post.create!(attributes) unless Post.where(attributes).first
+	end
+
+
+	 posts = Post.all
+	 
+	 # Create Comments
+	
+	 #100.times do
+	 #  Comment.create!(
+	  #   post: posts.sample,
+	 #    body: Faker::Lorem.paragraph
+	 #  )
+	 #end
+	 
+	 puts "Seed finished"
+	 puts "#{Post.count} posts created"
+	 puts "#{Comment.count} comments created"
