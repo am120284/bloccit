a298983ae12bb8eeebdfadf854bfa4a7fc432a6d
Adrian <Adrian@Kraferd.com>
2015-03-31 17:18:46 -0700

    Added Crud features to our web page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
-
-  get 'welcome/contact'
+  resources :posts
+  get 'about' => 'welcome#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
