40a5098530ea5c8566b5aec148f0880c46a30121
Adrian Munoz <am120284@gmail.com>
2015-05-07 14:08:25 -0700

    destroy status assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     #@topic    = Topic.find(params[:topic_id])
-    @post     =  Post.find(params[:post_id])
+    @post     = Post.find(params[:post_id])
     @comments = @post.comments
 
     #After separating the topics resources from the comments, Here in the Comments controller it cant find the topic
@@ -10,11 +10,10 @@ class CommentsController < ApplicationController
     @comment      = current_user.comments.build(comment_params)
     @comment.post = @post
 
-
     #authorize! :create, @comment, message: "You need be signed in to do that."
     if @comment.save
       flash[:notice] = "Comment was created."
-      redirect_to post_comments_path(@post, @comments)
+      redirect_to topic_post_path @post.topic, @post
     else
       flash[:error] = "There was an error saving the comment. Please try again."
       render 'posts/show'
@@ -22,15 +21,15 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @topic   =          Topic.find(params[:topic_id])
-    @post    =   @topic.posts.find(params[:post_id])
+    #@topic   =          Topic.find(params[:topic_id])
+    @post    =   Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
 
     authorize @comment
 
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
+      redirect_to topic_post_path @post.topic, @post
 
     else
       flash[:error]  = "Comment couldn't be deleted. Try again."
