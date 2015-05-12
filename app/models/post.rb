b85740eea333ebe5599016a7cc105c0ec8be9d38
Adrian <Adrian@Kraferd.com>
2015-05-12 11:48:51 -0700

    update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,52 @@
 class Post < ActiveRecord::Base
 	has_many   :comments, dependent: :destroy
   has_one    :summary
+  has_many   :votes, dependent: :destroy
 	belongs_to :user
 	belongs_to :topic
   mount_uploader :image,  ImageUploader
 
-	default_scope { order('created_at DESC') }
+	default_scope { order('rank DESC') }
 
 	validates :title, length: {minimum: 5 }, presence: true
 	validates :body,  length: {minimum: 20}, presence: true
-	validates :topic, presence: true
-	validates :user,  presence: true
+	#validates :topic, presence: true
+	#validates :user,  presence: true
+
+  #--------------------------Votes Functions--------------------------------
+    def up_votes
+      votes.where(value: 1).count
+    end
+
+    def down_votes
+      votes.where(value: -1).count
+    end
+
+
+    def points
+      votes.sum(:value)
+    end
+
+    def update_rank
+      age_in_days = ( created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
+      new_rank = points + age_in_days
+
+      update_attribute(:rank, new_rank)
+    end
+  #-----------------------End of Vote Functions-----------------------------
+
+
+    def create_vote
+      user.votes.create(value: 1, post: self)
+      # user.votes.create(value: 1, post: self)
+      # self.user.votes.create(value: 1, post: self)
+      # votes.create(value: 1, user: user)
+      # self.votes.create(value: 1, user: user)
+
+      # vote = Vote.create(value: 1, user: user, post: self)
+      # self.votes << vote
+      # save
+    end
 
   def markdown_title
     render_as_markdown title
@@ -24,9 +60,9 @@ class Post < ActiveRecord::Base
 
   def render_as_markdown(markdown)
 
-    renderer   =  Redcarpet::Render::HTML.new
-    extensions = {fenced_code_blocks: true }
-    redcarpet  =  Redcarpet::Markdown.new(renderer, extensions)
+     renderer   =  Redcarpet::Render::HTML.new
+     extensions = {fenced_code_blocks: true }
+     redcarpet  =  Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
 
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 
   has_many :posts
   has_many :comments
+  has_many :votes, dependent: :destroy
   mount_uploader :avatar, AvatarUploader
 
   devise :database_authenticatable, :registerable,
