09a451a7eca4454f7c62331561b7113de9ba384e
Adrian <Adrian@Kraferd.com>
2015-05-21 18:35:14 -0700

    popular post checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,17 @@ Rails.application.routes.draw do
 
     resources :summary
     devise_for :users
-    resources :users, only: [:update, :show]
+    resources :users, only: [:update, :show, :index]
 
     resources :topics do
-      resources :posts, except: [:index]
+      resources :posts, except: [:index], controller: 'topics/posts'
     end
 
-    resources :posts, only: [] do
-      resources :comments,  only: [:create, :destroy]
-      resources :favorites, only: [:create, :destroy]
-      post '/up-vote'   => 'votes#up_vote',   as: :up_vote
-      post '/down-vote' => 'votes#down_vote', as: :down_vote
+      resources :posts, only: [:index] do
+        resources :comments,  only: [:create, :destroy]
+        resources :favorites, only: [:create, :destroy]
+        post '/up-vote'   => 'votes#up_vote',   as: :up_vote
+        post '/down-vote' => 'votes#down_vote', as: :down_vote
     end
 
   
